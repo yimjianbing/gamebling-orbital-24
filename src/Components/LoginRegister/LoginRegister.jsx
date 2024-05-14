70dc41d981de85627a9dc86643fd1292cb597b00
jianbing <yeahitsyjb@gmail.cmom>
2024-05-15 01:31:03 +0800

    Added responsive CSS styling to formbox

--- src/Components/LoginRegister/LoginRegister.jsx
+++ src/Components/LoginRegister/LoginRegister.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React , {useState} from "react";
 import "./LoginRegister.css"
+import { HiMiniUser, HiMiniKey } from "react-icons/hi2";
+import { HiMail } from "react-icons/hi";
 
 const LoginRegister = () => {
+
+
+    const [action, setAction] = useState('');
+
+    const registerLink = () => {
+        setAction(' active');
+    }
+
+    const loginLink = () => {
+        setAction('');
+    }
+
+    
+
     return (
-        <div className ='wrapper'>
-            <div className="form=box login">
+        <div className ={`wrapper${action}`}>
+            <div className="form-box login">
                 <form action="">
                     <h1>Login</h1>
                     <div className="input-box">
-                        <input type="text" placeholder="Username" required />
+                        <input type="email" placeholder="Email" required />
+                        <HiMail className="icon"/>
                     </div>
                     <div className="input-box">
                         <input type="password" placeholder="Password" required />
+                        <HiMiniKey className="icon"/>
                     </div>
 
                     <div className="remember-forget">
@@ -23,8 +41,39 @@ const LoginRegister = () => {
                     <button type="submit">Login</button>
 
                     <div className="register-link">
-                        <p>Don't have an Account?
-                            <a href="#">Register</a>
+                        <p>Don't have an account?
+                            <a href="#" onClick={registerLink}>Register</a>
+                        </p>
+                    </div>
+                </form>
+            </div>
+
+            <div className="form-box register">
+                <form action="">
+                    <h1>Registration</h1>
+                    <div className="input-box">
+                        <input type="text" placeholder="Username" required />
+                        <HiMiniUser className="icon"/>
+                    </div>
+                    <div className="input-box">
+                        <input type="email" placeholder="Email" required />
+                        <HiMail className="icon"/>
+                    </div>
+                    <div className="input-box">
+                        <input type="password" placeholder="Password" required />
+                        <HiMiniKey className="icon"/>
+                    </div>
+
+                    <div className="remember-forget">
+                        <label><input type="checkbox" />
+                        I agree to the terms & conditions</label>
+                    </div>
+
+                    <button type="submit">Register</button>
+
+                    <div className="register-link">
+                        <p>Already have an account?
+                            <a href="#" onClick={loginLink}>Login</a>
                         </p>
                     </div>
                 </form>
