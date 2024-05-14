ab90895cd42e7b92d3398cbffdcc13819b2a4398
jianbing <yeahitsyjb@gmail.cmom>
2024-05-15 03:20:30 +0800

    Debuged and tidied up code

--- src/Components/LoginRegister/LoginRegister.jsx
+++ src/Components/LoginRegister/LoginRegister.jsx
@@ -1,152 +1,195 @@
-import React , {useState} from "react";
-import "./LoginRegister.css"
+import React, { useState } from "react";
+import "./LoginRegister.css";
 import { HiMiniUser, HiMiniKey } from "react-icons/hi2";
 import { HiMail } from "react-icons/hi";
-import { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "./firebase-config";
+import {
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "./firebase-config";
 import { validateField, validatePassword } from "./validators";
 
 const LoginRegister = () => {
+  const [action, setAction] = useState("");
 
+  const registerLink = () => {
+    setAction(" active");
+  };
 
-    const [action, setAction] = useState('');
+  const loginLink = () => {
+    setAction("");
+  };
 
-    const registerLink = () => {
-        setAction(' active');
+  //set up register using firebase auth
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = async (e) => {
+    e.preventDefault();
+
+    //Validate input fields
+    if (validatePassword(password) == false) {
+      alert("Invalid email/password!");
+      return;
+      //bad input
     }
 
-    const loginLink = () => {
-        setAction('');
+    if (validateField(name) == false) {
+      alert("One or more characters is needed for username!");
+      return;
     }
 
-    //set up register using firebase auth
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(function () {
+        alert("User Created!");
+      })
+      .catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage);
+      });
+  };
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  //set up login using firebase auth
 
-    const register = async(e) => {
-        e.preventDefault();
+  const login = async (e) => {
+    e.preventDefault();
 
-        //Validate input fields
-        if (validatePassword(password) == false) {
-            alert('Invalid email/password!');
-            return;
-            //bad input
-        }
-
-        if(validateField(name) == false) {
-            alert('One or more characters is needed for username!');
-            return;
-        }
-
-        createUserWithEmailAndPassword(auth, email, password)
-        .then(function() {
-    
-            alert("User Created!");
-    
-        })
-        .catch(function(error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            alert(errorMessage);
-        }); 
+    //Validate input fields
+    if (validatePassword(password) == false) {
+      alert("Invalid email/password!");
+      return;
+      //bad input
     }
 
-    //set up login using firebase auth
+    signInWithEmailAndPassword(auth, email, password)
+      .then(function () {
+        alert("User Logged in successful!");
+      })
+      .catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage);
+      });
+  };
 
+  return (
+    <div className={`wrapper${action}`}>
+      <div className="form-box login">
+        <form action="">
+          <h1 className="topOfBox">Login</h1>
+          <div className="input-box">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              required
+            />
+            <HiMail className="icon" size={24} />
+          </div>
+          <div className="input-box">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              required
+            />
+            <HiMiniKey className="icon" size={24} />
+          </div>
 
-    const login = async(e) => {
-        e.preventDefault();
+          <div className="remember-forgot">
+            <label>
+              <input type="checkbox" />
+              Remember me
+            </label>
+            <a href="#">Forgot password?</a>
+          </div>
 
-        //Validate input fields
-        if (validatePassword(password) == false) {
-            alert('Invalid email/password!');
-            return;
-            //bad input
-        }
+          <button
+            className="login-register-button"
+            type="submit"
+            onClick={login}
+          >
+            Login
+          </button>
 
-        if(validateField(name) == false) {
-            alert('One or more characters is needed for username!');
-            return;
-        }
+          <div className="register-link">
+            <p className="haveAccount">
+              Don't have an account?
+              <a href="#" onClick={registerLink}>
+                {" "}
+                Register
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
 
-        signInWithEmailAndPassword(auth, email, password)
-        .then(function() {
-    
-            alert("User Logged in successful!");
-    
-        })
-        .catch(function(error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            alert(errorMessage);
-        }); 
-    }
+      <div className="form-box register">
+        <form action="">
+          <h1 className="topOfBox">Registration</h1>
+          <div className="input-box">
+            <input
+              type="text"
+              value={name}
+              placeholder="Username"
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            <HiMiniUser className="icon" size={24} />
+          </div>
+          <div className="input-box">
+            <input
+              type="email"
+              value={email}
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <HiMail className="icon" size={24} />
+          </div>
+          <div className="input-box">
+            <input
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <HiMiniKey className="icon" size={24} />
+          </div>
 
-    
+          <div className="remember-forgot">
+            <label>
+              <input type="checkbox" />I agree to the terms & conditions
+            </label>
+          </div>
 
-    return (
-        <div className ={`wrapper${action}`}>
-            <div className="form-box login">
-                <form action="">
-                    <h1 className="topOfBox">Login</h1>
-                    <div className="input-box">
-                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
-                        <HiMail className="icon" size={24}/>
-                    </div>
-                    <div className="input-box">
-                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
-                        <HiMiniKey className="icon" size={24}/>
-                    </div>
+          <button
+            className="login-register-button"
+            type="submit"
+            onClick={register}
+          >
+            Register
+          </button>
 
-                    <div className="remember-forgot">
-                        <label><input type="checkbox" />
-                        Remember me</label>
-                        <a href="#">Forgot password?</a>
-                    </div>
-
-                    <button className="login-register-button" type="submit" onClick={login}>Login</button>
-
-                    <div className="register-link">
-                        <p className="haveAccount">Don't have an account?
-                            <a href="#" onClick={registerLink}> Register</a>
-                        </p>
-                    </div>
-                </form>
-            </div>
-
-            <div className="form-box register">
-                <form action="">
-                    <h1 className="topOfBox">Registration</h1>
-                    <div className="input-box">
-                        <input type="text" value={name} placeholder="Username" onChange={(e) => setName(e.target.value)} required />
-                        <HiMiniUser className="icon" size={24}/>
-                    </div>
-                    <div className="input-box">
-                        <input type="email" value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
-                        <HiMail className="icon" size={24}/>
-                    </div>
-                    <div className="input-box">
-                        <input type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
-                        <HiMiniKey className="icon" size={24}/>
-                    </div>
-
-                    <div className="remember-forgot">
-                        <label><input type="checkbox" />
-                        I agree to the terms & conditions</label>
-                    </div>
-
-                    <button className="login-register-button"type="submit" onClick={register}>Register</button>
-
-                    <div className="register-link">
-                        <p className="haveAccount">Already have an account?
-                            <a href="#" onClick={loginLink}> Login</a>
-                        </p>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
-}
+          <div className="register-link">
+            <p className="haveAccount">
+              Already have an account?
+              <a href="#" onClick={loginLink}>
+                {" "}
+                Login
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default LoginRegister;
--- src/Components/LoginRegister/validators.js
+++ src/Components/LoginRegister/validators.js
@@ -1,21 +1,21 @@
 export function validatePassword(password) {
-    // check if password is above 6 chars
-    if (password < 6) {
-        // alert("no!")
-        return false;
-    } else {
-        return true;
-    }
+  // check if password is above 6 chars
+  if (password < 6) {
+    // alert("no!")
+    return false;
+  } else {
+    return true;
+  }
 }
 
 export function validateField(field) {
-    if (field == null) {
-        return false;
-    }
+  if (field == null) {
+    return false;
+  }
 
-    if (field.length <= 0) {
-        return false;
-    }
+  if (field.length <= 0) {
+    return false;
+  }
 
-    return true;
+  return true;
 }
