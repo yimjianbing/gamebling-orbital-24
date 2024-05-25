9915b7804de35a9bd2cada3bbcf3227f2557ab97
Yim Jian Bing <yeahitsyjb@gmail.com>
2024-05-25 23:55:44 +0800

    deleted unused files and added images to mainmenu buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { useMemo } from "react";
 import RouterBuilder from "./Components/NavBar/Routing.jsx";
 import { AuthProvider } from "./auth/AuthContext.jsx";
-import MainMenu from "./Components/MainMenu/MainMenu.jsx";
 
 function App() {
   const routes = useMemo(() => RouterBuilder(), []);
