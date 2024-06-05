f2f2fd355683625c45e021b081a63feaed073ec2
Yim Jian Bing <yeahitsyjb@gmail.com>
2024-06-06 03:39:20 +0800

    changed ui to make the web app more mobile friendly

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -8,9 +8,9 @@ export function NavBar() {
     <header data-testid="navbar">
       <h2 className="logo">GameBling</h2>
       <nav className="navigation">
-        <Link to="/" data-testid="home">Home</Link>
-        <Link to="/about" data-testid="about">About</Link>
-        <Link to="/contactus" data-testid="contactus"> Contact Us</Link>
+        <Link to="/" data-testid="home"> Home </Link>
+        <Link to="/about" data-testid="about"> About </Link>
+        <Link to="/contactus" data-testid="contactus"> Contact Us </Link>
         <Link to="/mainmenu" className="btnLogin-popup" data-testid="mainmenu">Play</Link>
       </nav>
     </header>
